794fdbcda6ec165460fd5f769503096eea187382
Chamberlain <talktochambox@yahoo.com>
2018-02-16 12:06:01 +0100

    data description

---
+++ pkg/NTCPmodels/R/DVH.R
@@ -0,0 +1,7 @@
+#' DVH data for 365 patients
+#'
+#' The dataset contains dose and volumes of the rectum irradiated of 365 patients.
+#'@format A list with each element containing a dataframe, the first column is the dose the second column is the volume.
+#'@source \url{http://www.milanHosp.info/}
+"DVH"
+

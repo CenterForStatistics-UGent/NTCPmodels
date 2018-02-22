193c5fcb1fee5e2718d0df358088e7c3abffd82a
Chamberlain <talktochambox@yahoo.com>
2018-02-22 03:01:12 +0100

    remove functions from to their own files

--- pkg/NTCPmodels/R/NTCP.R
+++ pkg/NTCPmodels/R/NTCP.R
@@ -14,6 +14,7 @@
 #' fraction for each patient.
 #' @param  toxicity A vector of same length as the fractionation, contain toxicity status of
 #' all patients. Toxicity should be coded as 1 and no-toxicity coded as 0
+#' @param link The type of link function,should either be "logit" or "probit" link.
 #' @param  n A vector or numeric constant, representing the \code{n} parameter in the
 #' Kutcher and Burman EUD reduction scheme, and should lie between 0 and 1.
 #' The defualt value is \code{NULL}. If no \code{n} constant or vector is supplied, NTCP will
@@ -25,6 +26,8 @@
 #' The default starting values are \code{c(theta = 4, TD50 = 30, m = 1)}
 #' @return
 #' @examples
+#' fit<-NTCP(DVH,fractionation, toxicity,link="logit")
+#' summary(fit)
 
 
 
@@ -46,6 +49,8 @@ NTCP<-function(DVH,
   colnames(paras)<-c("n", "theta","TD_50","m")
   paras$n<-round(n,2)
 
+if(missing(fractionation))
+  fractionation<-rep(2,length(toxicity))
 
 for(kk in 1:len2)
   {
@@ -64,7 +69,7 @@ for(kk in 1:len2)
                               frac=fractionation,y=toxicity,FUN=plogis,
                               method="L-BFGS-B", control=list(trace=TRUE, REPORT=1),
                               hessian=TRUE,
-                              lower=c(0,0,-1000),upper=c(100,100,1000)),
+                              lower=c(0,0,-1000),upper=c(100,80,1000)),
                    error=function(cond) {
                      message(paste("EUD could not be computed for n =", n[kk]))
                      return(NA)
@@ -77,76 +82,13 @@ for(kk in 1:len2)
 
 }
   out<-list(modelInfor=paras,dataInfo=preDVH$EUD,toxicity=toxicity,fractionation=fractionation,link=link,n=n)
+  class(out)<-"NTCPmodels"
+  out
 
 }
 
 #Looklihood function
-llf<- function(param, EUD, frac,y,FUN) {
-  xbeta<- ((((frac+param[1])/(2+param[1]))*(1/param[2]))*EUD-1)*(1/param[3])
-  neglog<--sum(y*log(FUN(xbeta))+(1-y)*log(1-FUN(xbeta)))
-  neglog
-}
+
 #Gradient function
-gra.lf<- function(param, EUD, frac,y,FUN) {
-  #Not the most elegant way of doing things but it works
-  if(names(formals(FUN)[2]) =="mean")FUN1<-dnorm
-  if(names(formals(FUN)[2]) =="location")FUN1<-dlogis
-  xbeta<- ((((frac+param[1])/(2+param[1]))*(1/param[2]))*EUD-1)*(1/param[3])
-  d_m<--((((frac+param[1])/(2+param[1]))*(1/param[2]))*EUD-1)*(1/param[3]^2)
-  d_TD50<--((((frac+param[1])/(2+param[1]))*(1/param[2]^2))*EUD)*(1/param[3])
-  d_theta<-((((2-frac)/(2+param[1])^2)*(1/param[2]))*EUD)*(1/param[3])
-  g_m<--sum(y*FUN(xbeta)/(FUN1(xbeta)*d_m)-(1-y)*(1-FUN(xbeta))/(FUN1(xbeta)*d_m))
-  g_TD50<--sum(y*FUN(xbeta)/(FUN1(xbeta)*d_TD50)-(1-y)*(1-FUN(xbeta))/(FUN1(xbeta)*d_TD50))
-  inter<-y*FUN(xbeta)/(FUN1(xbeta)*d_theta)-(1-y)*(1-FUN(xbeta))/(FUN1(xbeta)*d_theta)
-  inter[frac==2]<-0
-  g_theta<--sum(inter)
-  grad<-c(g_theta,g_TD50,g_m)
-  grad
-}
-
-#Prepares dvh data for fitting
-prepare.DVH<-function(DVH,n=NULL)
-{
-  len<-length(DVH)
-  d<-matrix(rep(0,1000*len),ncol = len)
-  v<-matrix(rep(0,1000*len),nrow=len)
-  binSize<-rep(NA,len)
-  for(i in 1:len)
-  {
-    dvh<-DVH[[i]]
-    lenn<-length(dvh[,2])
-    v[i,][1:(lenn)]<-c(0,-diff(dvh[,2]/max(dvh[,2])))
-    grp<-diff(dvh[,1])
-    if(unique(grp)>1)stop(paste("The dose bin size of the observation",i,"has a problem"))
-    binSize[i]<-unique(grp)
-  }
-  binSize<-as.factor(binSize)
-  levs<-as.numeric(levels(binSize))
-  ll1<-length(levs)
-  doseBins<-matrix(rep(0,ll1*1000),ncol=ll1)
-  for(ii in 1:ll1)
-  {
-    seqq<-seq(levs[ii],100,by=levs[ii])
-    doseBins[,ii][1:length(seqq)]<-seqq
-  }
-  if(is.null(n))
-  {
-    len2<-30
-    n<-seq(0.01,1,length.out = len2)
-  }
-  else
-  {
-    if(sum(n<0 | n>1)>0)stop("n must lie within 0 and 1")
-    len2<-length(n)
-  }
 
 
-  EUDn<-matrix(rep(NA,len2*len),ncol=len2)
-  for(k in 1:len2)
-  {
-    for(j in 1:ll1)
-      EUDn[,k][binSize==levs[j]]<-(v[binSize==levs[j],]%*%(doseBins[,j]^(1/n[k])))^n[k]
-  }
-
-  list(EUD=EUDn,n=n)
-}

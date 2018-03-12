9be4ea28eb93cef63e197cce84c1021e45bacb8b
Chamberlain <talktochambox@yahoo.com>
2018-03-12 13:31:09 +0100

    a few minor adjustmensts

--- pkg/NTCPmodels/R/plot.cvNTCPmodels.R
+++ pkg/NTCPmodels/R/plot.cvNTCPmodels.R
@@ -1,9 +1,14 @@
 plot.cvNTCPmodels<-function(object)
 {
-  plot(as.numeric(unlist(object[1])),
-       as.numeric(unlist(object[2])),
+  xx<-as.numeric(object[[1]][,1])
+  yy<-as.numeric(object[[1]][,2])
+  if(object$type.measure=="auc")ylab="AUC"
+  plot(object[[1]],
        xlab = "n",cex.lab=1.3,type="b",
-       ylab = names(object)[2],
+       ylab = ylab,ylim=range(c(yy+qnorm(0.972)*object$sd,yy-qnorm(0.972)*object$sd)),
        pch=16,col="red")
+  arrows(xx,yy-qnorm(0.972)*object$sd,xx,yy+qnorm(0.972)*object$sd,length = 0.08,angle = 90,code = 3,col="gray")
+  abline(v=xx[yy==max(yy)],lty=2)
 }
+
 

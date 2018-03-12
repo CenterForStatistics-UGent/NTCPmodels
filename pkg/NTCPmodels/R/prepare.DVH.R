ee496be351b3a9779258a1f63d7a69651958f756
Chamberlain <talktochambox@yahoo.com>
2018-03-12 13:32:27 +0100

    critical made instead of using differential dvhs we use normal transformations
    using normal transformation on the DVH data to get normal quantiles

--- pkg/NTCPmodels/R/prepare.DVH.R
+++ pkg/NTCPmodels/R/prepare.DVH.R
@@ -5,24 +5,38 @@ prepare.DVH<-function(DVH,n=NULL)
   d<-matrix(rep(0,1000*len),ncol = len)
   v<-matrix(rep(0,1000*len),nrow=len)
   binSize<-rep(NA,len)
-  for(i in 1:len)
-  {
-    dvh<-DVH[[i]]
-    lenn<-length(dvh[,2])
-    v[i,][1:(lenn)]<-c(0,-diff(dvh[,2]/max(dvh[,2])))
-    grp<-diff(dvh[,1])
-    if(unique(grp)>1)stop(paste("The dose bin size of the observation",i,"has a problem"))
-    binSize[i]<-unique(grp)
-  }
-  binSize<-as.factor(binSize)
-  levs<-as.numeric(levels(binSize))
-  ll1<-length(levs)
-  doseBins<-matrix(rep(0,ll1*1000),ncol=ll1)
-  for(ii in 1:ll1)
-  {
-    seqq<-seq(levs[ii],100,by=levs[ii])
-    doseBins[,ii][1:length(seqq)]<-seqq
-  }
+  dvh<-lapply(DVH, function(x)
+    {
+    x[,2]<-1-(x[,2]/max(x[,2]))
+    x[,2]<-ifelse(x[,2]==0,1e-10,x[,2])
+    x[,2]<-ifelse(x[,2]==1,1-1e-10,x[,2])
+    x[,1]<-qnorm(x[,2])
+    x[,2]<-dnorm(x[,1])
+    x
+  })
+ #  par(mfrow=c(1,2))
+ # plot(dvh[[200]][,1],dvh[[200]][,2],type="l")
+ # lines(dvh[[6]][,1],dvh[[6]][,2],type="l")
+ #
+ # plot(DVH[[200]][,1], c(0,diff(1-(DVH[[200]][,2]/max(DVH[[200]][,2])))),type="l")
+ # lines(DVH[[6]][,1],c(0,diff(1-(DVH[[6]][,2]/max(DVH[[6]][,2])))),type="l")
+ #  for(i in 1:len)
+ #  {
+ #    dvh<-DVH[[i]]
+ #    lenn<-length(dvh[,2])
+ #    v[i,][1:(lenn)]<-c(0,-diff(dvh[,2]/max(dvh[,2])))
+ #    grp<-diff(dvh[,1])
+ #    if(unique(grp)>1)stop(paste("The dose bin size of the observation",i,"has a problem"))
+ #  }
+  # binSize<-as.factor(binSize)
+  # levs<-as.numeric(levels(binSize))
+  # ll1<-length(levs)
+  # doseBins<-matrix(rep(0,ll1*1000),ncol=ll1)
+  # for(ii in 1:ll1)
+  # {
+  #   seqq<-seq(levs[ii],100,by=levs[ii])
+  #   doseBins[,ii][1:length(seqq)]<-seqq
+  # }
   if(is.null(n))
   {
     len2<-30
@@ -34,14 +48,14 @@ prepare.DVH<-function(DVH,n=NULL)
     len2<-length(n)
   }
 
+#Normal transformation so we get uncorrelated odd moments
 
   EUDn<-matrix(rep(NA,len2*len),ncol=len2)
   for(k in 1:len2)
   {
-    for(j in 1:ll1)
-      EUDn[,k][binSize==levs[j]]<-(v[binSize==levs[j],]%*%(doseBins[,j]^(1/n[k])))^n[k]
-  }
 
+    EUDn[,k]<- unlist(lapply(dvh, function(x,k)(sum(((x[,1]+10)^(1/n[k]))*x[,2]))^n[k],k=k))
+  }
 maxEUD<-max(EUDn)
   list(EUD=EUDn/maxEUD
        ,maxEUD=maxEUD, n=n)
